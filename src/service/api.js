2bc0e4da694d724a3402e7ed63de80974950bac3
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-10-25 18:05:24 +0530

    .

--- src/service/api.js
+++ src/service/api.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 const Backend_Url = process.env.REACT_APP_BACKEND_URL;
-// http://localhost:5000
+
+const url = REACT_APP_BACKEND_URL_LOCAL|| REACT_APP_BACKEND_URL;
 export const addUser = async (data) => {
   try {
-    await axios.post(`http://localhost:5000/add`, data);
+    await axios.post(`${url}/add`, data);
   } catch (err) {
     console.log(err.message);
   }
 };
 export const getUsers = async () => {
   try {
-    let response = await axios.get(`http://localhost:5000/users`);
+    let response = await axios.get(`${url}/users`);
     // console.log(response.data)
     return response.data;
   } catch (error) {
@@ -19,14 +20,14 @@ export const getUsers = async () => {
 };
 export const setConversation = async (data) => {
   try {
-    await axios.post(`http://localhost:5000/conversation/add`, data);
+    await axios.post(`${url}/conversation/add`, data);
   } catch (err) {
     console.log(err.message);
   }
 };
 export const getConversation = async (data) => {
   try {
-    let response = await axios.post(`http://localhost:5000/conversation/get`, data);
+    let response = await axios.post(`${url}/conversation/get`, data);
     return response.data;
   } catch (err) {
     console.log(err.message);
@@ -35,7 +36,7 @@ export const getConversation = async (data) => {
 
 export const newMessage = async (data) => {
   try {
-    let response = await axios.post(`http://localhost:5000/message/add`, data);
+    let response = await axios.post(`${url}/message/add`, data);
     return response.data;
   } catch (err) {
     console.log(err.message);
@@ -43,7 +44,7 @@ export const newMessage = async (data) => {
 };
 export const getMessage = async (id) => {
   try {
-    let response = await axios.get(`http://localhost:5000/message/get/${id}`);
+    let response = await axios.get(`${url}/message/get/${id}`);
     return response.data;
   } catch (err) {
     console.log(err.message);
@@ -51,7 +52,7 @@ export const getMessage = async (id) => {
 };
 export const uploadFile = async (data) => {
   try {
-      return await axios.post(`http://localhost:5000/file/upload`, data);
+      return await axios.post(`${url}/file/upload`, data);
   } catch (error) {
       console.log('Error while calling file upload API ', error);
   }
