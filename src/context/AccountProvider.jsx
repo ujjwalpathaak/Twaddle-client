2d07f4ac94c912f2289a016bce44b2a5dac2a112
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-10-26 17:58:07 +0530

    .

--- src/context/AccountProvider.jsx
+++ src/context/AccountProvider.jsx
@@ -13,7 +13,7 @@ const AccountProvider = ({ children }) => {
   const socket = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:9000")
+    socket.current = io("https://twaddle-socket.herokuapp.com/")
   }, [])
   
   
