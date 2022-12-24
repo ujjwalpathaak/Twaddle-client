655b90cc55eb572b703740ee9a3a8aa39b1a2f92
Ujjwal Pathak <pathak2002ujjwal@gmail.com>
2022-12-25 03:02:47 +0530

    .

--- src/service/api.js
+++ src/service/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
-// "http://localhost:5000"
-// "http://localhost:5000"
+// "http://localhost:50f00/"
+// "http://localhost:50f00/"
 export const addUser = async (data) => {
   try {
-    await axios.post(`http://localhost:5000/add`, data);
+    await axios.post(`https://twaddle-server.onrender.com/add`, data);
   } catch (err) {
     console.log(err.message);
   }
 };
 export const getUsers = async () => {
   try {
-    let response = await axios.get(`http://localhost:5000/users`);
+    let response = await axios.get(`https://twaddle-server.onrender.com/users`);
     console.log("reached - get users")
     console.log(response.data)
     return response.data;
@@ -20,14 +20,14 @@ export const getUsers = async () => {
 };
 export const setConversation = async (data) => {
   try {
-    await axios.post(`http://localhost:5000/conversation/add`, data);
+    await axios.post(`https://twaddle-server.onrender.com/conversation/add`, data);
   } catch (err) {
     console.log(err.message);
   }
 };
 export const getConversation = async (data) => {
   try {
-    let response = await axios.post(`http://localhost:5000/conversation/get`, data);
+    let response = await axios.post(`https://twaddle-server.onrender.com/conversation/get`, data);
     return response.data;
   } catch (err) {
     console.log(err.message);
@@ -36,7 +36,7 @@ export const getConversation = async (data) => {
 
 export const newMessage = async (data) => {
   try {
-    let response = await axios.post(`http://localhost:5000/message/add`, data);
+    let response = await axios.post(`https://twaddle-server.onrender.com/message/add`, data);
     console.log(response)
     return response.data;
   } catch (err) {
@@ -45,7 +45,7 @@ export const newMessage = async (data) => {
 };
 export const getMessage = async (id) => {
   try {
-    let response = await axios.get(`http://localhost:5000/message/get/${id}`);
+    let response = await axios.get(`https://twaddle-server.onrender.com/message/get/${id}`);
     return response.data;
   } catch (err) {
     console.log(err.message);
@@ -53,7 +53,7 @@ export const getMessage = async (id) => {
 };
 export const uploadFile = async (data) => {
   try {
-      return await axios.post(`http://localhost:5000/file/upload`, data);
+      return await axios.post(`https://twaddle-server.onrender.com/file/upload`, data);
   } catch (error) {
       console.log('Error while calling file upload API ', error);
   }
